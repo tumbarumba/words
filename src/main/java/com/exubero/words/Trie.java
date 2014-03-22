5fb36e18882ed98dbb64808a94cd5b75324b7dde
Joe Schmetzer <joe@exubero.com>
2014-03-22 20:30:14 +0000

    Implemented the trie

--- src/main/java/com/exubero/words/Trie.java
+++ src/main/java/com/exubero/words/Trie.java
@@ -1,16 +1,66 @@
 package com.exubero.words;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Set;
+import java.util.List;
 
 public class Trie {
 
-    public void addWord(String word) {
+    private static final class TrieNode {
+        private final String character;
+        private final TrieNode[] children;
+        private boolean isWord = false;
 
+        public TrieNode(String c) {
+            children = new TrieNode[26];
+            character = c;
+        }
+
+        public void addWord(String word) {
+            char firstChar = word.charAt(0);
+            TrieNode childNode = childNodeFor(firstChar);
+            if (word.length() > 1) {
+                childNode.addWord(word.substring(1));
+            } else {
+                childNode.isWord = true;
+            }
+
+        }
+
+        private TrieNode childNodeFor(char firstChar) {
+            int charPos = firstChar - 'a';
+            if (children[charPos] == null) {
+                children[charPos] = new TrieNode(Character.toString(firstChar));
+            }
+            return children[charPos];
+        }
+
+        public void addWordsTo(List<String> words, String baseWord) {
+            String nodeWord = baseWord + character;
+            if (isWord) {
+                words.add(nodeWord);
+            }
+            for(TrieNode childNode : children) {
+                if (childNode != null) {
+                    childNode.addWordsTo(words, nodeWord);
+                }
+            }
+        }
     }
 
-    public Set<String> words() {
-        return Collections.emptySet();
+    private final TrieNode root = new TrieNode("");
+
+    public Trie() {
+    }
+
+    public void addWord(String word) {
+        root.addWord(word.toLowerCase());
+    }
+
+    public List<String> words() {
+        List<String> allWords = new ArrayList<String>();
+        root.addWordsTo(allWords, "");
+        return Collections.unmodifiableList(allWords);
     }
 
 }
