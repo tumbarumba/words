9a4faf46e2a461522e1269809b7e3ca49e0cc326
Joe Schmetzer <joe@exubero.com>
2014-03-24 01:07:25 +0000

    Implemented Main

--- src/main/java/com/exubero/words/Main.java
+++ src/main/java/com/exubero/words/Main.java
@@ -1,8 +1,28 @@
 package com.exubero.words;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Loading dictionary...");
+        WordDictionary dictionary = loadDictionary();
+        System.out.println("Looking up words for " + args[0]);
+        List<String> words = dictionary.findWordsUsing(args[0]);
+        System.out.println("Found:");
+        int count = 0;
+        for (String word : words) {
+            System.out.println("\t" + ++count + ":\t" + word);
+        }
+    }
+
+    private static WordDictionary loadDictionary() {
+        try {
+            return WordDictionary.loadFrom("/enable1.txt");
+        }
+        catch (IOException ex) {
+            throw new RuntimeException("Unable to load dictionary: " + ex.getMessage());
+        }
     }
 }
